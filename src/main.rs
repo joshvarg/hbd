7719be652cba1f040b3fa980feb88d8e5ca3ac6c
Joshua Varghese <joshvarg.sec@gmail.com>
2022-10-10 14:15:41 -0400

    build: add and use clap for argument parsing
    By using clap I am hoping for more scalability with code. When I
    start adding more features, I want to be able to introduce more
    subcommands without main being too convoluted.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::env;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
+use clap::Parser;
+
+#[derive(Parser,Default,Debug)]
+#[clap(author="Author Name", version, about)]
+/// Never be the family asshole again!
+struct Arguments {
+    name: String,
+    day: usize,
+    month: usize,
+    year: usize,
+}
 
 
-fn entry_add(name: &String, dob: &String) -> io::Result<File> {
+fn entry_add(name: &String, day: usize, month: usize, year: usize) -> io::Result<File> {
     let mut file = File::create("entry.txt").expect("Could not create file");
-    let output = format!("{} {}", name, dob);
+    let output = format!("{} has a birthday on {}-{}-{}\n", name, month, day, year);
     file.write_all(output.as_bytes()).expect("Could not write entry");
 
     Ok(file)
@@ -14,12 +24,13 @@ fn entry_add(name: &String, dob: &String) -> io::Result<File> {
 
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 3 {
-        println!("app <name> <DoB>");
-    } else {
-        let name = &args[1];
-        let dob = &args[2];
-        entry_add(name, dob).expect("Could not add entry");
-    }
+    let args = Arguments::parse();
+    //: Vec<_> = env::args().collect();
+    println!("{:?}", args);
+    let name = &args.name;
+    let day = args.day;
+    let month = args.month;
+    let year = args.year;
+    entry_add(name, day, month, year).expect("Could not add entry");
+    
 }
