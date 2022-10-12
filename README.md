c7fdf81def68b7116a6cfffe2473bf61232536dc
Joshua Varghese <joshvarg.sec@gmail.com>
2022-10-12 15:18:41 -0400

    feat: introduce subcommands: 'add' and 'list'
    Reorganized code so that clap can now leverage its subcommand features.
    TODOs are added that talk about finding relative directory/filepaths for
    data storage.
    
    Added README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# hbd
+helpful birthday documentor
+![image](https://img.shields.io/badge/status-wip-orange)
+## About
+
+Tired of being the jerk that can't remember anyone's birthdays'?
+
+hbd is here to help! hbd is a command line based tool to keep track of all those pesky birthdays.
+
+### Built With
+Rust
+
+## Getting Started
+
+### Prerequisites
+
+- Cargo
+- Rust
+
+### Installation
+1. Clone the repo
+> `git clone https://github.com/joshvarg/hbd.git`
+2. Build with Cargo
+> `cargo build`
+
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,55 @@
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
-use clap::Parser;
+use std::fs;
+use clap::{Parser, Subcommand};
+// TODO: use 'dirs' crate for actual path finding
 
-#[derive(Parser,Default,Debug)]
+#[derive(Parser)]
 #[clap(author="Author Name", version, about)]
 /// Never be the family asshole again!
-struct Arguments {
-    name: String,
-    day: usize,
-    month: usize,
-    year: usize,
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        name: String,
+        year: usize,
+        month: usize,
+        day: usize,
+    },
+    List,
 }
 
 
-fn entry_add(name: &String, day: usize, month: usize, year: usize) -> io::Result<File> {
-    let mut file = File::create("entry.txt").expect("Could not create file");
-    let output = format!("{} has a birthday on {}-{}-{}\n", name, month, day, year);
+fn entry_add(name: &String, day: &usize, month: &usize, year: &usize) -> io::Result<File> {
+    fs::create_dir_all("~/.bday/");
+    // TODO: fix this path to be the actual home of user
+    let mut file = File::create("~/.bday/entry.txt").expect("Could not create file");
+    let output = format!("{} has a birthday on {}-{}-{}\n", name, *month, *day, *year);
     file.write_all(output.as_bytes()).expect("Could not write entry");
 
     Ok(file)
 }
 
+fn list() -> String {
+    let contents = fs::read_to_string("~/.bday/entry.txt").expect("No birthdays logged in ~/.bday/ !");
+    // TODO: fix this path to be the actual home of user
+    return contents;
+}
+
 
 fn main() {
-    let args = Arguments::parse();
-    //: Vec<_> = env::args().collect();
-    println!("{:?}", args);
-    let name = &args.name;
-    let day = args.day;
-    let month = args.month;
-    let year = args.year;
-    entry_add(name, day, month, year).expect("Could not add entry");
-    
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Add { name, year, month, day } => {
+            entry_add(name, day, month, year).expect("Could not add entry");
+        }
+        Commands::List => {
+            println!("{}", list());
+        }
+    }
 }
